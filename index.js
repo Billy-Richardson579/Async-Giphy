87baaeaac3418eaf5c2d0f738b05eee19989d15f
Billy Richardson <billy7373731@outlook.com>
2023-05-02 15:04:06 +0100

    commit 1

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 require('dotenv').config();
 
+const apiKey = process.env.API_KEY;
+
 // Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+console.log(apiKey)
+async function getImage(query){
+const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}&limit=25&offset=0&rating=g&lang=en`
+
+const response = await fetch(endpoint)
+const data = await response.json();
+console.log(data)
+return data
+} 
